b160ba36120a6b492e2c544187a2fe2c8273eb6c
Olivia Zaleski <zalesol265@gmail.com>
2024-06-22 21:06:07 +0200

    Changing button style

--- app.py
+++ app.py
@@ -18,12 +18,7 @@ app.layout = dbc.Container([
             html.H1("Image Color Clustering"),
             dcc.Upload(
                 id='upload-image',
-                children=html.Div(['Drag and Drop or ', html.A('Select Files')]),
-                style={
-                    'width': '100%', 'height': '60px', 'lineHeight': '60px',
-                    'borderWidth': '1px', 'borderStyle': 'dashed', 'borderRadius': '5px',
-                    'textAlign': 'center', 'margin': '10px'
-                },
+                children=html.Div(['Upload image']),
                 multiple=False
             ),
             html.Label('Number of Colors:'),
